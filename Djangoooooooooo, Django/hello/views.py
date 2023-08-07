b82c5920e359f15aaa0b12b9397383ec945868fe
Vincent-Glen <vincentg130@gmail.com>
2023-08-07 14:53:36 +1200

    Django Website done

---
+++ Djangoooooooooo, Django/hello/views.py
@@ -0,0 +1,36 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def Shop(request):
+    return render(request, "hello/Shop.html")
+    return HttpResponse(content)
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
