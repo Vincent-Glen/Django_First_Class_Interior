84bc39b0163b4e74ce203e52957b77103f23ef7f
Vincent-Glen <vincentg130@gmail.com>
2023-08-15 22:13:46 +1200

    Reverted changes

--- Djangoooooooooo, Django/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hello import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-    path("Shop/", views.Shop, name="Shop"),
-]
